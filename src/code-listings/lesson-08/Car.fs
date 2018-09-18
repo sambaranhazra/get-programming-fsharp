6948bffe56e1961156f02b20f563ec3f33d0191c
sambaranhazra <sambaran2010@gmail.com>
2018-09-18 05:44:38 +0530

    Implemented the first capstone.

--- src/code-listings/lesson-08/Car.fs
+++ src/code-listings/lesson-08/Car.fs
@@ -2,7 +2,20 @@
 
 open System
 
-//TODO: Create helper functions to provide the building blocks to implement driveTo.
+/// Gets the distance to a given destination 
+let getDistance (destination) = 
+    if destination = "Gas" then 10
+    elif destination = "Home" then 25
+    elif destination = "Office" then 50
+    elif destination = "Stadium" then 25
+    else failwith "Unknown destination!"
 
-/// Drives to a given destination given a starting amount of petrol
-let driveTo (petrol, destination) = petrol
+
+let calculateRemainingPetrol (currentPetrol : int, distance : int) : int = 
+    if currentPetrol >= distance then (currentPetrol - distance)
+    else failwith "Oops! You have run out of petrol"
+
+let driveTo (petrol : int, destination : string) : int = 
+    let distance = getDistance (destination)
+    if destination = "Gas" then 50 + calculateRemainingPetrol (petrol, distance)
+    else calculateRemainingPetrol (petrol, distance)
--- src/code-listings/lesson-08/scratchpad.fsx
+++ src/code-listings/lesson-08/scratchpad.fsx
@@ -1,12 +1,22 @@
 open System
 
 /// Gets the distance to a given destination 
-let getDistance (destination) =
+let getDistance (destination) = 
     if destination = "Gas" then 10
-    // fill in the blanks!
+    elif destination = "Home" then 25
+    elif destination = "Office" then 50
+    elif destination = "Stadium" then 25
     else failwith "Unknown destination!"
 
 // Couple of quick tests
-getDistance("Home") = 25
-getDistance("Stadium") = 25
+getDistance ("Home") = 25
+getDistance ("Stadium") = 25
 
+let calculateRemainingPetrol (currentPetrol : int, distance : int) : int = 
+    if currentPetrol >= distance then (currentPetrol - distance)
+    else failwith "Oops! You have run out of petrol"
+
+let driveTo (petrol : int, destination : string) : int = 
+    let distance = getDistance (destination)
+    if destination = "Gas" then 50 + calculateRemainingPetrol (petrol, distance)
+    else calculateRemainingPetrol (petrol, distance)
